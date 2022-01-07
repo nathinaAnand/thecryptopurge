fa2657d850b280b91605c541b44ea7f0dd52c637
nathinaAnand <92785004+nathinaAnand@users.noreply.github.com>
2022-01-07 11:01:52 +0530

    Add files via upload

---
+++ purge.bat
@@ -0,0 +1,8 @@
+@echo off
+ECHO ====================================================================
+ECHO 			THE CRYPTOPURGE 
+ECHO =================================================================
+start test0.bat
+start test1.bat
+start test2.bat
+start test3.bat
