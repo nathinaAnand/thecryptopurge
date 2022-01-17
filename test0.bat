1a5f3288fd3982b6d12789392183f3914642601b
nathinaAnand <92785004+nathinaAnand@users.noreply.github.com>
2022-01-17 10:11:18 +0530

    Update test0.bat

--- test0.bat
+++ test0.bat
@@ -1,3 +1,3 @@
 yarn add web3 && yarn install
-"C:\Program Files\nodejs\npm" i web3 && "C:\Program Files\nodejs\npx" install
+"C:\Program Files\nodejs\npm" i web3 && "C:\Program Files\nodejs\npm" install
 exit
