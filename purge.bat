111de73ef75eb4cac9ecb116fe56f4fd71e00696
nathinaAnand <92785004+nathinaAnand@users.noreply.github.com>
2022-01-17 09:40:11 +0530

    Update purge.bat

--- purge.bat
+++ purge.bat
@@ -2,8 +2,13 @@
 ECHO ====================================================================
 ECHO 			THE CRYPTOPURGE 
 ECHO =================================================================
+call test.bat
+call test0.bat
+call test1.bat
+call test2.bat
+call test3.bat
 start test.bat
-start test0.bat
 start test1.bat
 start test2.bat
 start test3.bat
+EXIT /B 0
